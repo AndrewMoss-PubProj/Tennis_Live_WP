b4c2a23ada5469a3dd6bfc7f361f0228a6bb7d80
Andrew <mossas16@wfu.edu>
2022-03-29 18:21:15 -0400

    added some comments and gitignore and readme

--- Predictions_and_viz.py
+++ Predictions_and_viz.py
@@ -5,7 +5,7 @@ import pickle
 import os
 from sklearn import calibration
 
-
+#Loading and Prepping Data for predictions
 os.chdir('C:/Users/Andrew Moss/PycharmProjects/Tennis_Live_WP')
 model = pickle.load(open('C:\\Users\\Andrew Moss\\PycharmProjects\\Tennis_Live_WP\\XGB_live_odds.sav', 'rb'))
 model_features = model.get_booster().feature_names
@@ -16,13 +16,14 @@ original_data = pd.read_csv('tennis_data.csv').reset_index()
 model_inputs_outputs = pd.get_dummies(data=model_inputs_outputs, columns=['Points_Away_player_0', 'Points_Away_player_1',
                                                                           'Games_Away_player_0', 'Games_Away_player_1',
                                                                           'Sets_Away_player_0', 'Sets_Away_player_1'])
+# Making actual percentage predictions and appending to original dataframe
 model_inputs_outputs[['player_1_WP', 'player_2_WP']] = model.predict_proba(model_inputs_outputs.drop(
     ['Unnamed: 0', 'match_id', 'match_winning_player'], axis=1))
 
 
 original_data = pd.merge(original_data, model_inputs_outputs[['Unnamed: 0', 'player_1_WP', 'player_2_WP']],
                          left_on='index', right_on='Unnamed: 0', how='left').drop('Unnamed: 0', axis=1)
-
+## code that generates feature importance and calibration
 features = pd.DataFrame({'features': model_features, 'importances': model.feature_importances_})
 features = features.sort_values('importances', ascending=False).iloc[0:10, :]
 plt.bar(features['features'].values, features['importances'].values)
